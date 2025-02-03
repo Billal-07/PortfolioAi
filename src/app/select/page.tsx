233db5bf8d4733cd465c13516a61b9539d9a3b15
ShayanAhmed-0 <shayanlost1@gmail.com>
2025-02-03 05:08:37 +0500

    selectpage

--- src/app/select/page.tsx
+++ src/app/select/page.tsx
@@ -1,94 +1,78 @@
-'use client'
+"use client";
 import React, { useState } from "react";
-
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "@/components/ui/tabs"
-import Template1 from '../Template1/page'
-import Template2 from '../Template2/page'
-import Template3 from '../Template3/page'
+import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 function SelectTabs() {
+  const [selected, setSelected] = useState(0);
+  const router = useRouter()
   return (
-    <Tabs defaultValue="account" className="w-full max-w-[1200px] mx-auto p-4">
-      <TabsList className="text-purple-800 bg-slate-500  grid w-full grid-cols-2">
-        <TabsTrigger value="account">Default Templates</TabsTrigger>
-        <TabsTrigger value="password">AI Generated Templates</TabsTrigger>
-      </TabsList>
-      <TabsContent value="account">
-        <div className="flex justify-between gap-4 mt-4 pb-4">
+    <div className="">
+      <Tabs
+        defaultValue="account"
+        className="w-full  max-w-full mx-auto p-4 pt-10"
+      >
+        <TabsList className="text-purple-800 bg-slate-500  grid w-full grid-cols-2">
+          <TabsTrigger value="account">Default Templates</TabsTrigger>
+          <TabsTrigger value="password">AI Generated Templates</TabsTrigger>
+        </TabsList>
+        <TabsContent
+          value="account"
+          className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 h-[70vh]"
+        >
           {[1, 2, 3].map((index) => (
-            <Card key={index} className="flex-shrink-0 w-[300px]">
-              {/* <Template1 />  */}
-              {/* <Template2 />  */}
-              {/* <Template3 />  */}
-              <CardHeader>
-                <CardTitle>Account</CardTitle>
-                <CardDescription>Make changes to your account here. Click save when you're done.</CardDescription>
-              </CardHeader>
-              <CardContent className="space-y-2">
-                <div className="space-y-1">
-                  <Label htmlFor={`name-${index}`}>Name</Label>
-                  <Input id={`name-${index}`} defaultValue="Pedro Duarte" />
-                </div>
-                <div className="space-y-1">
-                  <Label htmlFor={`username-${index}`}>Username</Label>
-                  <Input id={`username-${index}`} defaultValue="@peduarte" />
-                </div>
-              </CardContent>
-              <CardFooter>
-                <Button>Save changes</Button>
-              </CardFooter>
-            </Card>
+            <div
+              key={index}
+              onClick={() => setSelected(index)}
+              className={`cursor-pointer hover:scale-105 duration-1000 relative w-full  ${
+                selected === index
+                  ? "border-purple-800 border-2 rounded-lg "
+                  : ""
+              }  min-h-[300px] `}
+            >
+              <Image
+                src={`/Templates/Template${index}.png`}
+                alt={`template${index}`}
+                layout="fill"
+                objectFit=""
+                className="rounded-lg p-1"
+              />
+            </div>
           ))}
+        </TabsContent>
+        <TabsContent value="password">
+          <div className="flex justify-center items-center -mt-[25rem] pb-20">
+            <p className="font-semibold text-3xl text-purple-600 font-mono ">
+              Coming Soon...
+            </p>
+          </div>
+        </TabsContent>
+      </Tabs>
+        <div className="flex justify-end mr-10">
+          <Button onClick={()=>router.push(`/Template${selected}`)} disabled={selected <= 0 || selected > 3} className="w-[20%]">
+      {/* <Link href={`/Template${selected}`}> */}
+            Next
+      {/* </Link> */}
+          </Button>
         </div>
-      </TabsContent>
-      <TabsContent value="password">
-        <Card className="mt-4">
-          <CardHeader>
-            <CardTitle>Password</CardTitle>
-            <CardDescription>Change your password here. After saving, you'll be logged out.</CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="current">Current password</Label>
-              <Input id="current" type="password" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="new">New password</Label>
-              <Input id="new" type="password" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>Save password</Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
-    </Tabs>
-  )
+    </div>
+  );
 }
 
 const SelectView: React.FC = () => {
-    return(
-        <div className="bg-gray-900 h-screen">
-            <div className="flex justify-center items-center h-screen">
-            <SelectTabs/>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="bg-gray-900 h-screen">
+      <div className="flex justify-center ">
+        <p className="font-semibold text-3xl text-purple-800 font-mono pt-10">
+          This is just an Idea Image Your Information will Be populated in it
+        </p>
+      </div>
+      <div className="">
+        <SelectTabs />
+      </div>
+    </div>
+  );
+};
 export default SelectView;
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,6 +10,8 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
+          "bg-purple-800 text-white shadow   hover:bg-purple-600/90",
+        shaddefault:
           "bg-primary text-primary-foreground shadow hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
